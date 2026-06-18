9b5a95817034deb588987327dbb9e0fd248f76dc
Marc Pidadade <map13jun@gmail.com>
2026-06-18 13:17:27 -0500

    Additional background shapes/Animations/Dark Mode

--- index.js
+++ index.js
@@ -2,6 +2,33 @@
 // serivce key: service_452pqhf
 // public key: mefr3HBKrPkw_e80L
 
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1/20;
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle
+    if (contrastToggle) {
+        document.body.classList += " dark-theme"
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+    
+}
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape")
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+
+    for (let i=0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+    }
+}
+
 function contact(event) {
     event.preventDefault();
     const loading = document.querySelector('.modal__overlay--loading')
@@ -30,7 +57,7 @@ function contact(event) {
     }, 1000);
 }
 
-let isModalOpen = false;
+
 function toggleModal() {
     if (isModalOpen) {
         isModalOpen = false;
